188cc54dd20fb67a09cb62ab11dcf38eb639f921
levyjonathan31 <jonathanlevy2002@gmail.com>
2023-10-01 03:57:43 -0400

    fixed image issue

--- app/JonathanLevy/page.jsx
+++ app/JonathanLevy/page.jsx
@@ -3,8 +3,8 @@ export default function Home() {
     <div className="bg-slate-300 text-black text-center">
       <h1 className="text-4xl font-bold mb-5">Informal Introuction</h1>
       <p>
-        {" "}
-        I wanted a place to give a more informal introduction to who I am.{" "}
+        I wanted a place to give a more informal introduction to who I am. Ever
+        since I was young,
       </p>
     </div>
   );
