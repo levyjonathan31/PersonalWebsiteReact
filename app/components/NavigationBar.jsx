4587fc10b8b8f5d180c588017cbcdd7e68dd5436
levyjonathan31 <jonathanlevy2002@gmail.com>
2023-10-01 04:19:59 -0400

    added dummy route to link

--- app/components/NavigationBar.jsx
+++ app/components/NavigationBar.jsx
@@ -39,10 +39,7 @@ function NavigationBar({ title, firstOption, secondOption, thirdOption }) {
         </div>
       </div>
       <div className="flex navbar-center">
-        <Link
-          // href="/JonathanLevy"
-          className="btn btn-ghost normal-case text-4xl"
-        >
+        <Link href="/" className="btn btn-ghost normal-case text-4xl">
           {title}
         </Link>
       </div>
